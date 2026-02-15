14d97a02f043037deca93b39953ec34495d05391
adeola brahim <brahimadeola2020@gmail.com>
2026-02-15 05:56:58 +0000

    create main class and allow user input

---
+++ java/0x00-java_foundation/number_base_converter/main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class main{
+    public static void main(String args[])
+    {
+        Scanner scn = new Scanner(System.in);
+        String num, src_base, target_base;
+
+        // prompt user for input
+        System.out.print("Enter number: ");
+        num = scn.nextLine();
+        System.out.print("Enter source base: ");
+        src_base = scn.nextLine();
+        System.out.print("Enter target base: ");
+        target_base = scn.nextLine();
+    }
+}
