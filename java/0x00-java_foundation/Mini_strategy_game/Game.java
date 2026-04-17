7840a66697854a212677d87d029393ea4ff53592
adeola brahim <brahimadeola2020@gmail.com>
2026-04-17 10:35:35 +0000

    add player actions

--- java/0x00-java_foundation/Mini_strategy_game/Game.java
+++ java/0x00-java_foundation/Mini_strategy_game/Game.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+
+
 public class Game
 {
     public static void main(String[] args)
@@ -9,5 +12,17 @@ public class Game
         player.army = 10;
 
         player.showStatus();
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("\nChoose an action");
+        System.out.println("1. Build Farm");
+        System.out.println("2. Train Army");
+        System.out.println("3. Skip");
+
+        int choice = input.nextInt();
+
+        System.out.println("You chose: " + choice);
+
     }
 }
