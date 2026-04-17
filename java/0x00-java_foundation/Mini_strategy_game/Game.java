9bcd72b9d4b1f60d0874131484e7f3be9ac83bc5
adeola brahim <brahimadeola2020@gmail.com>
2026-04-17 12:28:49 +0000

    add game loop to spawn 3 rounds

--- java/0x00-java_foundation/Mini_strategy_game/Game.java
+++ java/0x00-java_foundation/Mini_strategy_game/Game.java
@@ -11,18 +11,40 @@ public class Game
         player.food = 50;
         player.army = 10;
 
-        player.showStatus();
+        for (int rd = 1; rd <= 3; rd++)
+        {
+            System.out.println("\n\t\t===================");
+            System.out.println("ROUND " + rd);
+            System.out.println("\t\t====================");
 
-        Scanner input = new Scanner(System.in);
+            player.showStatus();
 
-        System.out.println("\nChoose an action");
-        System.out.println("1. Build Farm");
-        System.out.println("2. Train Army");
-        System.out.println("3. Skip");
+            Scanner input = new Scanner(System.in);
 
-        int choice = input.nextInt();
+            System.out.println("\nChoose an action");
+            System.out.println("1. Build Farm");
+            System.out.println("2. Train Army");
+            System.out.println("3. Skip");
+            
+            System.out.print("Pick an option: ");
+            int choice = input.nextInt();
 
-        System.out.println("You chose: " + choice);
+            if (choice == 1)
+            {
+                System.out.println("You built a farm!");
+                player.food += 20;
+            }
+            else if (choice == 2)
+            {
+                System.out.println("You trained soldiers!");
+                player.army += 5;
+                player.gold -= 20;
+            }
+            else
+                System.out.println("You skipped the turn");
 
+            System.out.println("\n\t\t\t****UPDATED STATUS****");
+            player.showStatus();
+        }
     }
 }
