bc62c07b7b1c91d81edf4436237ae37d5b2b3f54
adeola brahim <brahimadeola2020@gmail.com>
2026-05-09 07:02:54 +0000

    print hello world

---
+++ java/0x00-java_foundation/Hello.java
@@ -0,0 +1,12 @@
+/**
+ * Hello - prints Hello, java! on std
+ *
+ */
+
+public class Hello
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, Java!");
+    }
+}
