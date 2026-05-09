44f4f7c68edc2cfea812a0399345d0017313e346
adeola brahim <brahimadeola2020@gmail.com>
2026-05-09 08:02:04 +0000

    print variables of basic primitive types

---
+++ java/0x00-java_foundation/Var.java
@@ -0,0 +1,22 @@
+/**
+ * Var - declare and print variabkes of all primitive types
+ *
+ */
+
+class Var
+{
+    public void data_types()
+    {
+        int number = 37;
+        double height = 6.0;
+        char gender = 'M';
+        boolean inClass = true;
+        float percentage = 78.9f;
+
+        System.out.println("int: " + number);
+        System.out.println("double: " + height);
+        System.out.println("char: " + gender);
+        System.out.println("boolean: " + inClass);
+        System.out.println("percentage: " + percentage);
+    }
+}
