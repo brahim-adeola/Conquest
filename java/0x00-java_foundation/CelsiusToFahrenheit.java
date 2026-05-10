9a8333889e3cb3c9d1afa629c86df88588e9dd75
adeola brahim <brahimadeola2020@gmail.com>
2026-05-10 06:31:16 +0000

    convert celsius to fahrenheit

---
+++ java/0x00-java_foundation/CelsiusToFahrenheit.java
@@ -0,0 +1,13 @@
+/**
+ * CelsiusToFahrenheit - Convert Celsius to Fahrenheit
+ *
+ */
+
+public class CelsiusToFahrenheit
+{
+    public double convert(int c)
+    {
+        double f = (double) c * 9 / 5 + 32;
+        return f;
+    }
+}
