56991ff502ba84ef95ee05b17405c83ec5fa7a95
adeola brahim <brahimadeola2020@gmail.com>
2026-05-17 00:59:52 +0000

    use length()

---
+++ java/0x00-java_foundation/Strlen.java
@@ -0,0 +1,14 @@
+/**
+ * Strlen - print length of a string
+ *
+ */
+
+public class Strlen
+{
+    void printlen()
+    {
+        String name = "Hammed Adeola Ibrahim";
+
+        System.out.println(name.length());
+    }
+}
