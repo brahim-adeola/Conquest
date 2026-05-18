85efce5f86034574e4c278af27d29682779e217c
adeola brahim <brahimadeola2020@gmail.com>
2026-05-18 10:07:53 +0000

    1+1

---
+++ java/0x00-java_foundation/Stradd.java
@@ -0,0 +1,13 @@
+/**
+ * Stradd - Concatenate two strings with + 
+ * 
+ * Return: New string
+ */
+
+public class Stradd
+{
+    public String joinStr(String str1, String str2)
+    {
+        return str1 + str2;
+    }
+}
