8ae95ba308f50bba40cade0beec960cbfd9f0903
adeola brahim <brahimadeola2020@gmail.com>
2026-05-20 08:07:53 +0000

    are you my friend

---
+++ java/0x00-java_foundation/isString.java
@@ -0,0 +1,13 @@
+/**
+ * isString - Check if two given strings are same
+ *
+ * Return: 1 if true else 0
+ */
+
+public class isString
+{
+    public boolean checker(String s1, String s2)
+    {
+        return s1.equalsIgnoreCase(s2);
+    }
+}
